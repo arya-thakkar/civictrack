c5fc114cd8ff059ab7201c2367f0be3a303eed58
arya-thakkar <a.thakkar071006@gmail.com>
2026-02-28 22:11:35 +0530

    production urls added

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,14 @@ const app = express();
 
 http.globalAgent.maxSockets = Infinity;
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://civictrack-frontend.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
